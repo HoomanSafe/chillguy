c537bc898a6ada756da46fc682fd57e49d9d28a6
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-24 18:50:26 +0800

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="py-4 w-full absolute top-0">
       <div className="container flex justify-between items-center">
         <a href="" className="font-goodGame uppercase text-5xl">
-          defi do
+          ChillGuy Mascot
         </a>
 
         <ul className="hidden lg:flex items-center gap-10 justify-between font-goodGame text-2xl capitalize">
@@ -17,7 +17,7 @@ const Header = () => {
 
         <div className="w-[181px] h-[54px] font-goodGame px-8 py-4 bg-white rounded shadow justify-center items-center gap-2.5 inline-flex">
           <div className="text-black text-base font-normal leading-snug tracking-tight">
-            BUY DAFIDO
+            BUY CHILLGUY
           </div>
         </div>
       </div>
